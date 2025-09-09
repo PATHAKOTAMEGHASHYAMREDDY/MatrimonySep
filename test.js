b31a56fe734fb816b6228068a076c49848693694
shyam <meghashyamreddy014@gmail.com>
2025-09-09 16:02:20 +0530

    Hi

--- test.js
+++ test.js
@@ -8,8 +8,5 @@ console.log("HI-Hello-1");
 console.log("HI-Hello-2");
 console.log("SIMPLE");
 console.log("SIMPLE-1");
-<<<<<<< HEAD
 console.log("SIMPLE-2");
 console.log("SIMPLE-3");
-=======
->>>>>>> d2526474fd6ad6c45f2511694aed4ffc381a484a
